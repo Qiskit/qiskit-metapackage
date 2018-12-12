6bcf8fd1eb76ac4c6842c7a8901426080ff175de
Salvador de la Puente González <salva@unoyunodiez.com>
2018-12-13 00:26:24 +0100

    Initial commit for the qiskit metapackage

---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+from setuptools import setup
+
+requirements = [
+    "qiskit-terra==0.7",
+    "qiskit-aer==0.7",
+]
+
+setup(
+    name="qiskit",
+    version="0.7.0",
+    description="Software for developing quantum computing programs",
+    long_description="""Qiskit is a software development kit for writing
+        quantum computing experiments, programs, and applications. Works with
+        Python 3.5 and 3.6""",
+    url="https://github.com/Qiskit/qiskit",
+    author="Qiskit Development Team",
+    author_email="qiskit@us.ibm.com",
+    license="Apache 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="qiskit sdk quantum",
+    packages=[
+        "qiskit",
+        "qiskit.terra",
+        "qiskit.aqua"
+    ],
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.5"
+)
