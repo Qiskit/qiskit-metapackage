62a20ab296f2ab48b22d678ce8080d8fe68d744d
Jay Gambetta <jay.gambetta@us.ibm.com>
2018-12-12 19:35:26 -0500

    Update __init__.py

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -10,11 +10,11 @@ terra and aer."""
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from qiskit.terra import QuantumCircuit
-from qiskit.terra import QuantumRegister
-from qiskit.terra import ClassicalRegister
+from qiskit.terra.circuit import QuantumCircuit
+from qiskit.terra.circuit import QuantumRegister
+from qiskit.terra.circuit import ClassicalRegister
 from qiskit.terra import Aer
 from qiskit.terra import IBMQ
 from qiskit.terra import QiskitError
-from qiskit.terra import compile
-from qiskit.terra import execute
+from qiskit.terra.tools.compiler import compile
+from qiskit.terra.tools.compiler import execute
