95399a346e08b888ad6779967e6bf4a61b0e2eb0
Marco Pistoia <pistoia@us.ibm.com>
2019-02-06 17:15:10 -0500

    AUTHORS listed alphabetically by last name; Qiskit.bib generated from script

--- tools/generate_bibtex.py
+++ tools/generate_bibtex.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-with open('AUTHORS', 'r') as authors_file:
-    authors = list(sorted([x.strip() for x in authors_file],
-                          key=lambda x: x.split()[-1]))
+with open('../AUTHORS', 'r') as authors_file:
+    authors = list([x.rstrip() for x in authors_file])
 
-with open('Qiskit.bib', 'w') as fd:
+with open('../Qiskit.bib', 'w') as fd:
     fd.write("@misc{ Qiskit,\n")
     fd.write('       author = {%s},\n' % ' and '.join(authors))
-    fd.write('       title = {Qiskit an opensource framework for quantum computing},\n')
-fd.write('       year = {2019},\n}\n')
+    fd.write('       title = {Qiskit: An Open-source Framework for Quantum Computing},\n')
+    fd.write('       year = {2019},\n}')
