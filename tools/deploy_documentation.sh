10073d362aa1baddd9fd9be9ede24a7c78103b22
Diego M. Rodriguez <diego.plan9@gmail.com>
2019-02-08 15:42:06 +0100

    Add script for deploying documentation

---
+++ tools/deploy_documentation.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+# Script for pushing the documentation to the qiskit.org repository.
+
+# Non-travis variables used by this script.
+TARGET_REPOSITORY="git@github.com:Qiskit/qiskit.org.git"
+TARGET_DOC_DIR="documentation"
+SOURCE_DOC_DIR="docs/_build/html"
+SOURCE_DIR=`pwd`
+
+# Build the documentation.
+#make doc
+
+# Setup the deploy key.
+# https://gist.github.com/qoomon/c57b0dc866221d91704ffef25d41adcf
+set -e
+openssl aes-256-cbc -K $encrypted_19594d4cf7cb_key -iv $encrypted_19594d4cf7cb_iv \
+     -in github_deploy_key.enc -out github_deploy_key -d
+chmod 600 github_deploy_key
+eval $(ssh-agent -s)
+ssh-add github_deploy_key
+
+# Clone the landing page repository.
+cd ..
+git clone --depth 1 $TARGET_REPOSITORY tmp
+cd tmp
+git config user.name "Qiskit Autodeploy"
+git config user.email "qiskit@qiskit.org"
+
+# Selectively delete files from the dir, for preserving versions and languages.
+#git rm -rf $TARGET_DOC_DIR/*.html \
+#    $TARGET_DOC_DIR/_* \
+#    $TARGET_DOC_DIR/autodoc \
+#    $TARGET_DOC_DIR/aqua \
+#    $TARGET_DOC_DIR/terra
+
+# Copy the new rendered files and add them to the commit.
+#cp -r $SOURCE_DIR/$SOURCE_DOC_DIR/* $TARGET_DOC_DIR/
+#git add $TARGET_DOC_DIR
+
+# Add a dummy file.
+echo "dummy" > $TARGET_DOC_DIR/DUMMY
+git add $TARGET_DOC_DIR/DUMMY
+
+# Commit and push the changes.
+git commit -m "Automated documentation update from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+#git push --quiet https://$GH_TOKEN@github.com/$TARGET_REPOSITORY_USER/$TARGET_REPOSITORY_NAME.git > /dev/null 2>&1
+git push
