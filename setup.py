26a3eb4a2bbca51a5f9ea531a0fdb873ed4ddd77
Matthew Treinish <mtreinish@kortar.org>
2019-02-25 08:45:02 -0500

    Pin versions and document versioning policy
    This commit pins the versions of the tracked elements to ensure that we
    only ever install a single version of an element when we install the
    meta-package. Additionally, to make it clear why we do this, and how we
    handle versioning this also adds a doc on the procedure for handling
    versioning.
    
    Fixes #141

--- setup.py
+++ setup.py
@@ -12,11 +12,10 @@ from setuptools import setup
 from setuptools.command.install import install
 from setuptools.command.develop import develop
 
-qiskit_terra = "qiskit-terra>=0.7,<0.8"
 
 requirements = [
-    qiskit_terra,
-    "qiskit-aer>=0.1,<0.2",
+    "qiskit_terra==0.7.0",
+    "qiskit-aer==0.1.1",
 ]
 
 
