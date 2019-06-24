284f97b4f7490ad225f5135561db6e5b4909bdb0
Matthew Treinish <mtreinish@kortar.org>
2019-06-24 17:29:02 -0400

    IBMQ too

--- tools/generate_authors.py
+++ tools/generate_authors.py
@@ -27,7 +27,8 @@ def main(repos=None, output_path=None):
                  'https://github.com/Qiskit/qiskit-ignis',
                  'https://github.com/Qiskit/qiskit-chemistry',
                  'https://github.com/Qiskit/qiskit-jku-provider',
-                 'https://github.com/Qiskit/qiskit']
+                 'https://github.com/Qiskit/qiskit',
+                 'https://github.com/Qiskit/qiskit-ibmq-provider']
     if not output_path:
         output_path = 'AUTHORS'
     authors = []
