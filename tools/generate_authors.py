c6b60ccb4340b01ac752fb60161defe96f67e5a5
Matthew Treinish <mtreinish@kortar.org>
2019-06-24 17:25:29 -0400

    Add the Qiskit/qiskit meta-repo to generate_authors.py
    We were not tracking the contributions to the Qiskit/qiskit
    meta-repository which is where we host all the documentation,
    benchmarks, and anything else that is used between elements. This
    commit corrects the oversight adds the Qiskit/qiskit repo to the list
    of repositories to use for generating the authors list.

--- tools/generate_authors.py
+++ tools/generate_authors.py
@@ -26,7 +26,8 @@ def main(repos=None, output_path=None):
                  'https://github.com/Qiskit/qiskit-aqua',
                  'https://github.com/Qiskit/qiskit-ignis',
                  'https://github.com/Qiskit/qiskit-chemistry',
-                 'https://github.com/Qiskit/qiskit-jku-provider']
+                 'https://github.com/Qiskit/qiskit-jku-provider',
+                 'https://github.com/Qiskit/qiskit']
     if not output_path:
         output_path = 'AUTHORS'
     authors = []
