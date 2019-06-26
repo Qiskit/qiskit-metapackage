611560691bb7b4d7a788e9d264351ab1d67d4f39
Shelly Garion <shelly@il.ibm.com>
2019-06-26 16:21:52 +0300

    updated benchmark test for RB

--- test/benchmarks/randomized_benchmarking.py
+++ test/benchmarks/randomized_benchmarking.py
@@ -56,30 +56,31 @@ def build_rb_circuit(nq, nseeds=1, length_vector=None,
 
 
 class RandomizedBenchmarkingBenchmark:
-    # parameters for RB:
-    # number of qubits
-    nq = 2
-    # number of seeds (random sequences)
-    nseeds = 20
-    # number of Cliffords in the sequence (start, stop, steps)
-    length_vector = np.arange(1, 200, 4)
-
-    params = (nq, nseeds, length_vector)
-    param_names = ['nq', 'nseeds', 'length_vector']
+    # parameters for RB (1&2 qubits):
+    params = ([1, 2, 3], [1, 5, 10],
+              [[[0]], [[0, 1]], [[0, 2], [1]]],
+              [[1], [1], [1, 3]],
+              [np.arange(1, 200, 4), np.arange(1, 500, 10)])
+    param_names = ['nq', 'nseeds', 'rb_pattern',
+                   'length_multiplier', 'length_vector']
+    versions = 2
     timeout = 600
 
-    def setup(self, nq, nseeds, length_vector):
+    def setup(self, nq, nseeds, length_vector, rb_pattern,
+              length_multiplier):
         random_seed = np.random.seed(10)
         self.circuit = build_rb_circuit(nq=nq, nseeds=nseeds,
                                         length_vector=length_vector,
+                                        rb_pattern=rb_pattern,
+                                        length_multiplier=length_multiplier,
                                         seed=random_seed)
 
         self.sim_backend = QasmSimulatorPy()
 
-    def time_simulator_transpile(self, _, __):
+    def time_simulator_transpile(self, _, __, ___, ____, _____):
         transpile(self.circuit, self.sim_backend)
 
-    def time_ibmq_backend_transpile(self, _, __):
+    def time_ibmq_backend_transpile(self, _, __, ___, ____, _____):
         # Run with ibmq_16_melbourne configuration
         coupling_map = [[1, 0], [1, 2], [2, 3], [4, 3], [4, 10], [5, 4],
                         [5, 6], [5, 9], [6, 8], [7, 8], [9, 8], [9, 10],
