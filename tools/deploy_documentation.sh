28ace7c468f02b2dd94479ca191961d8dbc1b4d2
SooluThomas <soolu.elto@gmail.com>
2019-08-26 14:19:01 -0400

    Test for syncing master and poBranch

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -26,6 +26,12 @@ SOURCE_REPOSITORY="git@github.com:Qiskit/qiskit.git"
 TARGET_BRANCH_PO="poBranch"
 DOC_DIR_PO="docs/locale"
 
+echo "pwd"
+pwd
+
+echo "git branch"
+git branch
+
 git fetch -p origin
 git merge origin/master
 git checkout $TARGET_BRANCH_PO
@@ -33,71 +39,71 @@ git merge master
 git push origin $TARGET_BRANCH_PO
 echo "***************** Merged master with poBranch! **********************"
 
-git checkout master
+# git checkout master
 
-# Build the documentation.
-make doc
+# # Build the documentation.
+# make doc
 
-cd docs
+# cd docs
 
-# Extract document's translatable messages into pot files
-# https://sphinx-intl.readthedocs.io/en/master/quickstart.html
-sphinx-build -b gettext -D language=$TRANSLATION_LANG . _build/gettext
+# # Extract document's translatable messages into pot files
+# # https://sphinx-intl.readthedocs.io/en/master/quickstart.html
+# sphinx-build -b gettext -D language=$TRANSLATION_LANG . _build/gettext
 
-# Setup / Update po files
-sphinx-intl update -p _build/gettext -l en
+# # Setup / Update po files
+# sphinx-intl update -p _build/gettext -l en
 
-# Setup the deploy key.
-# https://gist.github.com/qoomon/c57b0dc866221d91704ffef25d41adcf
-set -e
-openssl aes-256-cbc -K $encrypted_19594d4cf7cb_key -iv $encrypted_19594d4cf7cb_iv \
-     -in ../tools/github_deploy_key.enc -out github_deploy_key -d
-chmod 600 github_deploy_key
-eval $(ssh-agent -s)
-ssh-add github_deploy_key
+# # Setup the deploy key.
+# # https://gist.github.com/qoomon/c57b0dc866221d91704ffef25d41adcf
+# set -e
+# openssl aes-256-cbc -K $encrypted_19594d4cf7cb_key -iv $encrypted_19594d4cf7cb_iv \
+#      -in ../tools/github_deploy_key.enc -out github_deploy_key -d
+# chmod 600 github_deploy_key
+# eval $(ssh-agent -s)
+# ssh-add github_deploy_key
 
-# Clone to the working repository for .po and pot files
-cd ..
-git clone --depth 1 $SOURCE_REPOSITORY temp --single-branch --branch $TARGET_BRANCH_PO
-cd temp
-git branch
-git config user.name "Qiskit Autodeploy"
-git config user.email "qiskit@qiskit.org"
+# # Clone to the working repository for .po and pot files
+# cd ..
+# git clone --depth 1 $SOURCE_REPOSITORY temp --single-branch --branch $TARGET_BRANCH_PO
+# cd temp
+# git branch
+# git config user.name "Qiskit Autodeploy"
+# git config user.email "qiskit@qiskit.org"
 
-# Remove old po files
-git rm -rf --ignore-unmatch $DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/*.po \
-    $DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/_*
+# # Remove old po files
+# git rm -rf --ignore-unmatch $DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/*.po \
+#     $DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/_*
 
-# Copy the new rendered files and add them to the commit.
-cp -r $SOURCE_DIR/$DOC_DIR_PO/ docs/
+# # Copy the new rendered files and add them to the commit.
+# cp -r $SOURCE_DIR/$DOC_DIR_PO/ docs/
 
-# git checkout translationDocs
-git add $DOC_DIR_PO
-ls
+# # git checkout translationDocs
+# git add $DOC_DIR_PO
+# ls
 
-# Commit and push the changes.
-git commit -m "Automated documentation update to add .po files from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-git push --quiet origin $TARGET_BRANCH_PO
-echo "********** End of pushing po to working repo! *************"
+# # Commit and push the changes.
+# git commit -m "Automated documentation update to add .po files from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+# git push --quiet origin $TARGET_BRANCH_PO
+# echo "********** End of pushing po to working repo! *************"
 
-# Clone the landing page repository.
-cd ..
-git clone --depth 1 $TARGET_REPOSITORY tmp
-cd tmp
-git config user.name "Qiskit Autodeploy"
-git config user.email "qiskit@qiskit.org"
+# # Clone the landing page repository.
+# cd ..
+# git clone --depth 1 $TARGET_REPOSITORY tmp
+# cd tmp
+# git config user.name "Qiskit Autodeploy"
+# git config user.email "qiskit@qiskit.org"
 
-# Selectively delete files from the dir, for preserving versions and languages.
-git rm -rf --ignore-unmatch $TARGET_DOC_DIR/*.html \
-    $TARGET_DOC_DIR/_* \
-    $TARGET_DOC_DIR/apidoc \
-    $TARGET_DOC_DIR/api
+# # Selectively delete files from the dir, for preserving versions and languages.
+# git rm -rf --ignore-unmatch $TARGET_DOC_DIR/*.html \
+#     $TARGET_DOC_DIR/_* \
+#     $TARGET_DOC_DIR/apidoc \
+#     $TARGET_DOC_DIR/api
 
-# Copy the new rendered files and add them to the commit.
-mkdir -p $TARGET_DOC_DIR
-cp -r $SOURCE_DIR/$SOURCE_DOC_DIR/* $TARGET_DOC_DIR/
-git add $TARGET_DOC_DIR
+# # Copy the new rendered files and add them to the commit.
+# mkdir -p $TARGET_DOC_DIR
+# cp -r $SOURCE_DIR/$SOURCE_DOC_DIR/* $TARGET_DOC_DIR/
+# git add $TARGET_DOC_DIR
 
-# Commit and push the changes.
-git commit -m "Automated documentation update from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-git push --quiet
+# # Commit and push the changes.
+# git commit -m "Automated documentation update from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+# git push --quiet
