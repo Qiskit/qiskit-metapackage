3289c0511e504ce3d74acb57d06b15226151909b
SooluThomas <soolu.elto@gmail.com>
2019-08-26 15:51:38 -0400

    Revert "Set-up automated deploy"
    This reverts commit 15a982a99ec52eb0b64cdc07ba9723607ff7eada.

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -19,66 +19,18 @@ TARGET_REPOSITORY="git@github.com:Qiskit/qiskit.org.git"
 TARGET_DOC_DIR="documentation"
 SOURCE_DOC_DIR="docs/_build/html"
 SOURCE_DIR=`pwd`
-SOURCE_LANG='en'
-TRANSLATION_LANG='ja'
-
-SOURCE_REPOSITORY="git@github.com:Qiskit/qiskit.git"
-TARGET_BRANCH_PO="poBranch"
-DOC_DIR_PO="docs/locale"
-
-git fetch -p origin
-git merge origin/master
-git checkout $TARGET_BRANCH_PO
-git merge master
-git push origin $TARGET_BRANCH_PO
-echo "***************** Merged master with poBranch! **********************"
-
-git checkout master
 
 # Build the documentation.
 make doc
-
-cd docs
-
-# Extract document's translatable messages into pot files
-# https://sphinx-intl.readthedocs.io/en/master/quickstart.html
-sphinx-build -b gettext -D language=$TRANSLATION_LANG . _build/gettext
-
-# Setup / Update po files
-sphinx-intl update -p _build/gettext -l en
 
 # Setup the deploy key.
 # https://gist.github.com/qoomon/c57b0dc866221d91704ffef25d41adcf
 set -e
 openssl aes-256-cbc -K $encrypted_19594d4cf7cb_key -iv $encrypted_19594d4cf7cb_iv \
-     -in ../tools/github_deploy_key.enc -out github_deploy_key -d
+     -in tools/github_deploy_key.enc -out github_deploy_key -d
 chmod 600 github_deploy_key
 eval $(ssh-agent -s)
 ssh-add github_deploy_key
-
-# Clone to the working repository for .po and pot files
-cd ..
-git clone --depth 1 $SOURCE_REPOSITORY temp --single-branch --branch $TARGET_BRANCH_PO
-cd temp
-git branch
-git config user.name "Qiskit Autodeploy"
-git config user.email "qiskit@qiskit.org"
-
-# Remove old po files
-git rm -rf --ignore-unmatch $DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/*.po \
-    $DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/_*
-
-# Copy the new rendered files and add them to the commit.
-cp -r $SOURCE_DIR/$DOC_DIR_PO/ docs/
-
-# git checkout translationDocs
-git add $DOC_DIR_PO
-ls
-
-# Commit and push the changes.
-git commit -m "Automated documentation update to add .po files from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-git push --quiet origin $TARGET_BRANCH_PO
-echo "********** End of pushing po to working repo! *************"
 
 # Clone the landing page repository.
 cd ..
