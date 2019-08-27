9931848adc661508f78eb43a3ab4c513b09edf7a
Matthew Treinish <mtreinish@kortar.org>
2019-08-27 12:31:03 -0400

    Fix deploy docs script poBranch update (#508)
    * Fix deploy docs script poBranch update
    
    This commit fixes the deploy documentation script trying to push to
    poBranch. Previously the the script was trying to reuse the deploy
    key [1] for pushing to qiskit.org, this will not work because the deploy
    key is only authorized to push to the Qiskit/qiskit.org repository. To
    enable pushing updated po files on merges to master a second deploy key
    is added with write permission to Qiskit/qiskit. This key is added to
    the ssh agent as part of the deploy documentation script so when we push
    to poBranch the user will have permission.
    
    At the same time some of the debugging setup in this script is replaced
    with turning bash trace mode on, this is easier than manually echoing
    command prior to running them.
    
    * Remove trace mode, it's a potential security risk
    
    * Update deploy_documentation.sh
    
    Removed redundant code and added code to git remove `/api` and `/apidocs` whenever `.po` files are updated

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -27,14 +27,11 @@ TARGET_BRANCH_PO="poBranch"
 DOC_DIR_PO="docs/locale"
 
 # Build the documentation.
-echo "make doc"
 make doc
-echo "end of make doc"
 
 echo "show current dir: "
 pwd
 
-echo "cd docs"
 cd docs
 
 # Extract document's translatable messages into pot files
@@ -46,24 +43,18 @@ sphinx-build -b gettext -D language=$TRANSLATION_LANG . _build/gettext
 echo "Setup / Update po files"
 sphinx-intl update -p _build/gettext -l en
 
-cd docs
-
-# Extract document's translatable messages into pot files
-# https://sphinx-intl.readthedocs.io/en/master/quickstart.html
-sphinx-build -b gettext -D language=$TRANSLATION_LANG . _build/gettext
-
-# Setup / Update po files
-sphinx-intl update -p _build/gettext -l en
-
-# Setup the deploy key.
-# https://gist.github.com/qoomon/c57b0dc866221d91704ffef25d41adcf
-echo "set ssh"
+echo "Setup ssh keys"
 pwd
 set -e
+# Add qiskit.org push key to ssh-agent
 openssl aes-256-cbc -K $encrypted_19594d4cf7cb_key -iv $encrypted_19594d4cf7cb_iv -in ../tools/github_deploy_key.enc -out github_deploy_key -d
 chmod 600 github_deploy_key
 eval $(ssh-agent -s)
 ssh-add github_deploy_key
+# Add poBranch push key to ssh-agent
+openssl enc -aes-256-cbc -d -in ../tools/github_poBranch_update_key.enc -out github_poBranch_deploy_key -K $encrypted_deploy_po_branch_key -iv $encrypted_deploy_po_branch_iv
+chmod 600 github_poBranch_deploy_key
+ssh-add github_poBranch_deploy_key
 
 # Clone to the working repository for .po and pot files
 cd ..
@@ -78,6 +69,8 @@ git config user.email "qiskit@qiskit.org"
 echo "git rm -rf for the translation po files"
 # git rm -rf --ignore-unmatch $DOC_DIR_2/$TRANSLATION_LANG/**/*.po # Remove old po files
 git rm -rf --ignore-unmatch $DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/*.po \
+	$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/api \
+	$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/apidoc \
 	$DOC_DIR_PO/$SOURCE_LANG/LC_MESSAGES/_*
 
 # Copy the new rendered files and add them to the commit.
@@ -87,13 +80,11 @@ cp -r $SOURCE_DIR/$DOC_DIR_PO/ docs/
 # git checkout translationDocs
 echo "add to po files to target dir"
 git add $DOC_DIR_PO
-ls
 
 # Commit and push the changes.
 git commit -m "Automated documentation update to add .po files from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
 echo "git push"
 git push --quiet origin $TARGET_BRANCH_PO
-ls
 echo "********** End of pushing po to working repo! *************"
 
 # Clone the landing page repository.
