a3849753a2441224a0d3c5befd231d1b41c35957
SooluThomas <soolu.elto@gmail.com>
2019-09-19 14:40:57 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
     'sphinx.ext.viewcode',
     'sphinx.ext.extlinks',
     'sphinx_tabs.tabs',
