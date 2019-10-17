1704fa2a8a778b0993b6e6a8096e54ef3069629e
SooluThomas <soolu.elto@gmail.com>
2019-10-17 16:48:05 -0400

    Cleaning up

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_theme/']
+templates_path = ['theme/']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -142,12 +142,11 @@ extlinks = {
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-# html_theme = 'sphinx_materialdesign_theme' # use the theme in subdir 'theme'
 
 html_theme = "sphinx_rtd_theme"
 
-html_theme_path = ["_theme", ]
+# html_theme_path = ["_theme", ]
+html_theme_path = ['.', sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -167,12 +166,27 @@ html_theme_options = {
     'titles_only': False
 }
 
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_theme/static/']
+html_static_path = ['theme/static/']
 
-html_favicon = '_theme/img/favicon.ico'
+# See https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html for details
+html_context = {
+    'github_url': "https://github.com/Qiskit/qiskit",
+    'github_user': "Qiskit",
+    'github_repo': "qiskit",
+    'github_version': "master",
+    'conf_py_path': "/docs/",
+    'display_github': True,
+    'css_files': [
+        '_static/css/theme-override.css',  # overrides few css in RTD Theme
+    ]
+}
+
+html_favicon = 'theme/img/favicon.ico'
+
 
 html_last_updated_fmt = '%Y/%m/%d'
 
