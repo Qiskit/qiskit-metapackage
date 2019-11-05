a308809d851a792211b4f82c4f721fc35a285962
SooluThomas <soolu.elto@gmail.com>
2019-11-04 19:12:22 -0500

    Update tools/deploy_documentation.sh
    Co-Authored-By: Matthew Treinish <mtreinish@kortar.org>

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -30,7 +30,7 @@ set -e
 
 # Clone the sources files and po files to $SOURCE_DIR/docs_source
 git clone --depth=1 $SOURCE_REPOSITORY docs_source
-rclone sync -v --exclude='locale/**' docs_source/docs docs/
+rclone sync -v --exclude='locale/**' docs_source/docs docs
 
 pushd $SOURCE_DIR/docs
 
