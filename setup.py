7f3208ea2fa926f8e762955b81a294a4c9de0f21
Diego M. Rodriguez <diego.plan9@gmail.com>
2019-11-11 12:30:35 +0100

    Bump versions

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ from setuptools import setup
 requirements = [
     "qiskit-terra==0.10.0",
     "qiskit-aer==0.3.2",
-    "qiskit-ibmq-provider==0.3.3",
+    "qiskit-ibmq-provider==0.4.0",
     "qiskit-ignis==0.2.0",
     "qiskit-aqua==0.6.1",
 ]
 
 setup(
     name="qiskit",
-    version="0.13.0",
+    version="0.14.0",
     description="Software for developing quantum computing programs",
     long_description="Qiskit is a software development kit for writing "
                      "quantum computing experiments, programs, and "
