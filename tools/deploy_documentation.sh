35f0ff904dee83160d1f3786d587fba6e8f56aa4
SooluThomas <soolu.elto@gmail.com>
2020-01-14 16:23:34 -0500

    Push po files to master branch

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -15,14 +15,11 @@
 # Script for pushing the documentation to the qiskit.org repository.
 
 # Non-travis variables used by this script.
-TARGET_REPOSITORY="git@github.com:Qiskit/qiskit.org.git"
-TARGET_DOC_DIR="documentation"
+SOURCE_REPOSITORY="git@github.com:Qiskit/qiskit.git"
 SOURCE_DOC_DIR="docs/_build/html"
 SOURCE_DIR=`pwd`
 SOURCE_LANG='en'
 
-SOURCE_REPOSITORY="git@github.com:Qiskit/qiskit.git"
-TARGET_BRANCH_PO="poBranch"
 DOC_DIR_PO="docs/locale"
 
 curl https://downloads.rclone.org/rclone-current-linux-amd64.deb -o rclone.deb
@@ -34,7 +31,7 @@ RCLONE_CONFIG_PATH=$(rclone config file | tail -1)
 tox -edocs -- -D content_prefix=documentation
 
 echo "show current dir: "
-pwd
+# pwd
 
 pushd docs
 
@@ -44,7 +41,7 @@ echo "Extract document's translatable messages into pot files and generate po fi
 tox -egettext -- -D language=$SOURCE_LANG
 
 echo "Setup ssh keys"
-pwd
+# pwd
 set -e
 # Add poBranch push key to ssh-agent
 openssl enc -aes-256-cbc -d -in ../tools/github_poBranch_update_key.enc -out github_poBranch_deploy_key -K $encrypted_deploy_po_branch_key -iv $encrypted_deploy_po_branch_iv
@@ -54,11 +51,11 @@ ssh-add github_poBranch_deploy_key
 
 # Clone to the working repository for .po and pot files
 popd
-pwd
-echo "git clone for working repo"
-git clone --depth 1 $SOURCE_REPOSITORY temp --single-branch --branch $TARGET_BRANCH_PO
-pushd temp
-git branch
+# pwd
+# echo "git clone for working repo"
+# git clone --depth 1 $SOURCE_REPOSITORY temp --single-branch --branch $TARGET_BRANCH_PO
+# pushd temp
+# git branch
 git config user.name "Qiskit Autodeploy"
 git config user.email "qiskit@qiskit.org"
 
@@ -79,16 +76,15 @@ rm -rf $SOURCE_DIR/$DOC_DIR_PO/en/LC_MESSAGES/api/ \
 echo "copy directory"
 cp -r $SOURCE_DIR/$DOC_DIR_PO/ docs/
 
-# git checkout translationDocs
 echo "add to po files to target dir"
 git add $DOC_DIR_PO
 
 # Commit and push the changes.
 git commit -m "Automated documentation update to add .po files from meta-qiskit" -m "[skip travis]" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
 echo "git push"
-git push --quiet origin $TARGET_BRANCH_PO
+git push --quiet origin master
 echo "********** End of pushing po to working repo! *************"
-popd
+# popd
 
 
 # Push to qiskit.org website
