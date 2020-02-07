4caf7e0e788954d453a4492f0b52e97481e8169c
Matthew Treinish <mtreinish@kortar.org>
2020-02-07 15:07:32 -0500

    Update package metadata in setup.py (#803)
    * Update python package long_description to use readme
    
    Right now the meta-package's description page is a short string which is
    hardcoded in the setup.py. This string is also currently out of date
    with the recent release of 0.15.0 which included python 3.8 support
    (although the trove classifier correctly indicates this). To provide
    more information about the package this changes the package description
    to be the README's contents. It will be less likely to go stale since
    there's only a single place to update and it will give the same view on
    pypi as we have on github.
    
    * Add project urls to package metadata
    
    To add useful links on the sidebar of the pypi page for the project this
    commit adds project urls to the package metadata. On the next qiskit
    meta-package release individual links to the source code, bug tracker,
    and rendered documentation will be added to the pypi package page.
    
    * Add missing README variable
    
    * Add missing import
    
    Co-authored-by: Kevin Krsulich <kevin@krsulich.net>

--- setup.py
+++ setup.py
@@ -12,7 +12,15 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+import os
+
 from setuptools import setup
+
+
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                           'README.md')
+with open(README_PATH) as readme_file:
+    README = readme_file.read()
 
 
 requirements = [
@@ -27,9 +35,8 @@ setup(
     name="qiskit",
     version="0.15.0",
     description="Software for developing quantum computing programs",
-    long_description="Qiskit is a software development kit for writing "
-                     "quantum computing experiments, programs, and "
-                     "applications. Works with Python 3.5, 3.6, and 3.7",
+    long_description=README,
+    long_description_content_type='text/markdown',
     url="https://github.com/Qiskit/qiskit",
     author="Qiskit Development Team",
     author_email="qiskit@us.ibm.com",
@@ -50,6 +57,11 @@ setup(
     ],
     keywords="qiskit sdk quantum",
     install_requires=requirements,
+    project_urls={
+        "Bug Tracker": "https://github.com/Qiskit/qiskit/issues",
+        "Documentation": "https://qiskit.org/documentation/",
+        "Source Code": "https://github.com/Qiskit/qiskit",
+    },
     include_package_data=True,
     python_requires=">=3.5",
 )
