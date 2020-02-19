2ea70a18c8544567be7e9177f7a1ab8c1c563fb6
Matthew Treinish <mtreinish@kortar.org>
2020-02-19 12:33:48 -0500

    Split translation doc publish jobs for each language
    Right now we're stuck in a spot where the docs builds can't be run in
    parallel locally and doing 2 language builds serially exceeds the
    maximum time allowed for a travis job. This commit tries to workaround
    this by splitting the jobs up so we have an individual job for each
    language. This should avoid the parallel build constraint and avoid us
    hitting the job time limit.

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -19,7 +19,6 @@ SOURCE_REPOSITORY="https://github.com/Qiskit/qiskit.git"
 TARGET_DOC_DIR="documentation/locale/"
 SOURCE_DOC_DIR="docs/_build/html/locale"
 SOURCE_DIR=`pwd`
-TRANSLATION_LANG="ja_JP de_DE"
 
 curl https://downloads.rclone.org/rclone-current-linux-amd64.deb -o rclone.deb
 sudo apt-get install -y ./rclone.deb
@@ -36,14 +35,11 @@ pushd $SOURCE_DIR/docs
 
 # Make translated document
 
-for i in ${TRANSLATION_LANG[@]}; do
-   echo $i;
-   sphinx-build -b html -D content_prefix=documentation -D language=$i . _build/html/locale/$i
-done
+sphinx-build -b html -D content_prefix=documentation -D language=$TRANSLATION_LANG . _build/html/locale/$TRANSLATION_LANG
 
 popd
 
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in tools/rclone.conf.enc -out $RCLONE_CONFIG_PATH -d
 
 echo "Pushing built docs to website"
-rclone sync ./docs/_build/html/locale IBMCOS:qiskit-org-website/documentation/locale
+rclone sync ./docs/_build/html/locale/$TRANSLATION_LANG IBMCOS:qiskit-org-website/documentation/locale/$TRANSLATION_LANG
