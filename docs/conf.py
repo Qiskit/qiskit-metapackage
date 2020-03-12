7d65e94aaf3daecba3ff1b9685a4e4544822394b
Paul Nation <nonhermitian@gmail.com>
2020-03-12 07:28:54 -0400

    remove txt suffix

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,7 @@ extensions = [
 ]
 
 nbsphinx_timeout = 60
+html_sourcelink_suffix = ''
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # Add any paths that contain templates here, relative to this directory.
