f656c5f280e747b5f1956c16114aecf154fa76d0
SooluThomas <soolu.elto@gmail.com>
2020-03-16 20:38:27 -0400

    Fix Lints

--- test/benchmarks/pulse/schedule_construction.py
+++ test/benchmarks/pulse/schedule_construction.py
@@ -30,7 +30,9 @@ def build_schedule(my_pulse, number_of_unique_pulses, number_of_channels):
 def sample_pulse(number_of_unique_pulses, number_of_channels):
     my_pulse = Gaussian(duration=25, sigma=4, amp=0.5j)
 
-    return (build_schedule(my_pulse, number_of_unique_pulses, number_of_channels))
+    return (build_schedule(my_pulse,
+                           number_of_unique_pulses,
+                           number_of_channels))
 
 
 # def parametric_pulse(self, number_of_unique_pulses, number_of_channels):
@@ -44,16 +46,19 @@ class ScheduleConstructionBench:
 
     def setup(self, number_of_unique_pulses, number_of_channels):
         self.empty_schedule = sample_pulse(number_of_unique_pulses, 0)
-        self.sample_schedule = sample_pulse(number_of_unique_pulses, number_of_channels)
+        self.sample_schedule = sample_pulse(number_of_unique_pulses,
+                                            number_of_channels)
 
-    def track_append_instruction(self, number_of_unique_pulses, number_of_channels):
+    def track_append_instruction(self,
+                                 number_of_unique_pulses,
+                                 number_of_channels):
         self.sample_schedule.append(self.sample_schedule)
 
     def track_insert_instruction_left_to_right():
         if self.sample_schedule.stop_time >= self.sample_schedule.start_time:
-            sample_schedule = self.sample_schedule.shift(self.sample_schedule.stop_time)
+            sched = self.sample_schedule.shift(self.sample_schedule.stop_time)
 
-        self.sample_schedule.insert(sample_schedule.start_time, sample_schedule)
+        self.sample_schedule.insert(sched.start_time, sched)
 
     # def instruction_to_schedule():
     #     pass
