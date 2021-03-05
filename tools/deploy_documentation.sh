cf518841eaec5a759f10e238292531c68b3cfd7c
SooluThomas <soolu.elto@gmail.com>
2021-03-05 07:48:49 -0500

    Fix typo (#1183)

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -29,4 +29,4 @@ pwd
 # Push to qiskit.org website
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in tools/rclone.conf.enc -out $RCLONE_CONFIG_PATH -d
 echo "Pushing built docs to website"
-rclone sync --progress ---exclude-from ./tools/other-builds.txt ./docs/_build/html IBMCOS:qiskit-org-web-resources/documentation
+rclone sync --progress --exclude-from ./tools/other-builds.txt ./docs/_build/html IBMCOS:qiskit-org-web-resources/documentation
