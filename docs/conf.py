1994248e987799f62fb5cd07d22a040da4bbc363
Eric Arellano <ericarellano@me.com>
2023-05-12 10:10:26 -0600

    Also remove CustomCardItemDirective

--- docs/conf.py
+++ docs/conf.py
@@ -26,11 +26,7 @@ sys.path.insert(0, os.path.abspath("."))
 
 import custom_extensions
 import url_redirects
-from custom_directives import (
-    CustomCalloutItemDirective,
-    CustomCardItemDirective,
-    IncludeDirective,
-)
+from custom_directives import CustomCalloutItemDirective, IncludeDirective
 
 # -- General configuration ---------------------------------------------------
 
@@ -179,7 +175,6 @@ plot_html_show_formats = False
 
 def setup(app):
     app.add_directive("includenodoc", IncludeDirective)
-    app.add_directive("customcarditem", CustomCardItemDirective)
     app.add_directive("customcalloutitem", CustomCalloutItemDirective)
     custom_extensions.load_api_sources(app)
     custom_extensions.load_tutorials(app)
