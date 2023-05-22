233735723a2c45ea1de0ad61f3ba4aed64afdadf
Eric Arellano <ericarellano@me.com>
2023-05-22 12:42:27 -0600

    Remove sphinx-redirects in favor of Cloudflare config

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # This code is part of Qiskit.
 #
 # (C) Copyright IBM 2018.
@@ -27,8 +25,9 @@ import sphinx.ext.doctest
 sys.path.insert(0, os.path.abspath("."))
 
 import custom_extensions
-import url_redirects
 from custom_directives import CustomCalloutItemDirective, CustomCardItemDirective
+import versionutils
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -56,13 +55,10 @@ extensions = [
     "sphinx.ext.extlinks",
     "nbsphinx",
     "sphinx_design",
-    "sphinx_reredirects",
     "matplotlib.sphinxext.plot_directive",
     "qiskit_sphinx_theme",
     "sphinx.ext.doctest",
 ]
-
-redirects = url_redirects.determine_redirects()
 
 nbsphinx_timeout = 300
 nbsphinx_execute = os.getenv("QISKIT_DOCS_BUILD_TUTORIALS", "never")
@@ -211,6 +207,6 @@ def setup(app):
     app.add_directive("customcarditem", CustomCardItemDirective)
     custom_extensions.load_api_sources(app)
     custom_extensions.load_tutorials(app)
-    app.setup_extension("versionutils")
+    versionutils.setup(app)
     app.connect("build-finished", custom_extensions.clean_api_source)
     app.connect("build-finished", custom_extensions.clean_tutorials)
