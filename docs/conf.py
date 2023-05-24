3415c0e68c024bb25891a186690fcba45826a2c9
Eric Arellano <ericarellano@me.com>
2023-05-24 11:48:31 -0600

    Fix loading of versionutils

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ sys.path.insert(0, os.path.abspath("."))
 
 import custom_extensions
 import url_redirects
+import versionutils
 
 # -- General configuration ---------------------------------------------------
 
@@ -219,6 +220,6 @@ doctest_test_doctest_blocks = ""
 def setup(app):
     custom_extensions.load_api_sources(app)
     custom_extensions.load_tutorials(app)
-    app.setup_extension("versionutils")
+    versionutils.setup(app)
     app.connect("build-finished", custom_extensions.clean_api_source)
     app.connect("build-finished", custom_extensions.clean_tutorials)
