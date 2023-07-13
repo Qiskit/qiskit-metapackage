Qiskit/qiskit-metapackage

commit c591e8ff3ba35a1b74203f1fe0a3bb4eed6399a8

Remove unused docs images (#1749)
* Remove unused docs images

* Move images from _static to images/

---------

Co-authored-by: Luciano Bello <bel@zurich.ibm.com>

docs/conf.py
@@ -151,7 +151,6 @@ html_theme_options = {
     "includehidden": True,
     "titles_only": False,
 }
-html_static_path = ["_static"]
 html_favicon = "images/favicon.ico"
 html_last_updated_fmt = "%Y/%m/%d"
 html_context = {

commit d8107cea6dff64db4d85711214a7ef5feeb6c720

Upgrade to qiskit-sphinx-theme 1.13 (#1772)
The main change is the new `qiskit` theme based on Furo, but we don't
yet activate that.

Otherwise, the only relevant change is simplifying the API for custom
directives. And fixing 404 errors about missing Thebe files.

--

This also fixes the images not working for two `qiskit-card`s, which I
accidentally broke in
https://github.com/Qiskit/qiskit-metapackage/pull/1749. For
`qiskit-card`, the images apparently must live in the `_static` folder
for Sphinx to know to include the images in the final build. This is
unlike when we normally use images.

docs/conf.py
@@ -88,6 +88,8 @@ panels_css_variables = {
     "tabs-color-label-active": "rgb(138, 63, 252)",
     "tabs-color-label-inactive": "rgb(221, 225, 230)",
 }
+
+html_static_path = ["_static"]
 templates_path = ["_templates"]
 
 source_suffix = ".rst"